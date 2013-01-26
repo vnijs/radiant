7c4399f28a905e81548b41065a7c84763fce3490
mostly-harmless <mostly.harmless.71@gmail.com>
2013-01-25 22:32:11 -0800

    Repos removed

--- inst/marketing/global.R
+++ inst/marketing/global.R
@@ -3,7 +3,7 @@ options(repos = c("http://vnijs.rady.ucsd.edu/site_media/R_packages/", "http://c
 libs <- c("shiny", "shinyIncubator", "car", "tools", "foreign", "ggplot2", "gridExtra", "R.utils", "plyr", "psych")
 available <- suppressWarnings(sapply(libs, require, character.only=TRUE))
 libs <- libs[available == FALSE]
-if(length(libs) != 0) install.packages(libs, dependencies = TRUE, repos = repos)
+if(length(libs) != 0) install.packages(libs, dependencies = TRUE)
 
 # setting up a few standard datasets to play with 
 mtcars$vs <- as.factor(mtcars$vs)
