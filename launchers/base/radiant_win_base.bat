cb134a0622f3c57e39cc53ca9461f2f23bdba646
mostly-harmless <vnijs@ucsd.edu>
2014-09-26 04:38:10 -0700

    Creating more flexible launchers using and R script

--- launchers/base/radiant_win_base.bat
+++ launchers/base/radiant_win_base.bat
@@ -1,4 +1,2 @@
 set radrpath=C:\Program Files\R\R-3.1.1\bin\R.exe
-set radpath=%HOMEDRIVE%%HOMEPATH%/Dropbox/radiant/inst/base/
-set radpath=%radpath:\=/%
-"%radrpath%" -e "Sys.setlocale(category = 'LC_ALL','English_United States.1252'); setwd('%radpath%'); source('../../radiant-miniCRAN/dependencies.R'); shiny::runApp('../base', port = 4000, launch.browser=TRUE)"
+"%radrpath%" -e "source('https://www.dropbox.com/s/bbch3xuyy7z4c6w/finding_radiant_app.R?dl=0'); shiny::runApp('inst/base', port = 4000, launch.browser=TRUE)"
