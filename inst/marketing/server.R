1e1745d42ce0830e116304fd87cc6d8c04a2a61c
mostly-harmless <vnijs@ucsd.edu>
2014-09-25 21:24:17 -0700

    Cleanup

---
+++ inst/marketing/server.R
@@ -0,0 +1,15 @@
+shinyServer(function(input, output, session) {
+
+	# source shared functions
+	source('../base/radiant.R', local = TRUE)
+
+	# source data & app tools from base
+	R.utils::sourceDirectory('../base/tools/app', recursive = TRUE, modifiedOnly = FALSE)
+	R.utils::sourceDirectory('../base/tools/data', recursive = TRUE, modifiedOnly = FALSE)
+
+	# source analysis tools from quant
+	R.utils::sourceDirectory('../quant/tools/analysis', recursive = TRUE, modifiedOnly = FALSE)
+
+	# source additional analysis tools for marketing app
+	R.utils::sourceDirectory('tools/analysis', recursive = TRUE, modifiedOnly = FALSE)
+})
