d27f484a843ddb01ac79cf79a3ad62b594d1fc24
mostly-harmless <vnijs@ucsd.edu>
2014-09-27 16:59:06 -0700

    Sync to dropbox shouldn't delete the .dropbox file

--- radiant-sync/sync2dropbox.sh
+++ radiant-sync/sync2dropbox.sh
@@ -1,2 +1,2 @@
-#rsync -avruz --exclude-from 'exclude-from-sync2dropbox.txt' ~/Desktop/radiant_dev/ ~/Dropbox/radiant/
-rsync -avruz --delete --exclude-from 'exclude-from-sync2dropbox.txt' ~/Desktop/radiant_dev/ ~/Dropbox/radiant/
+#rsync -avruz --delete --exclude-from 'exclude-from-sync2dropbox.txt' ~/Desktop/radiant_dev/ ~/Dropbox/radiant/
+rsync -avruz --exclude-from 'radiant-sync/exclude-from-sync2dropbox.txt' ~/Desktop/radiant_dev/ ~/Dropbox/radiant/
