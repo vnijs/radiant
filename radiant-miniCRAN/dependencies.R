a94bdccc0bcf5979dfc748af3ee091b446dfa5ff
mostly-harmless <vnijs@ucsd.edu>
2014-09-28 13:17:17 -0700

    Radiant on Shiny Server

--- radiant-miniCRAN/dependencies.R
+++ radiant-miniCRAN/dependencies.R
@@ -1,4 +1,5 @@
 # install packages required to run radiant
+# next line checks if Radiant is being run locally or not
 if(Sys.getenv('SHINY_PORT') == "") {
 
   if(file.exists("~/Desktop/radiant_dev/")) {
@@ -10,38 +11,32 @@ if(Sys.getenv('SHINY_PORT') == "") {
       mcran <- paste0("file:///",normalizePath("~/../Dropbox/radiant/radiant-miniCRAN", winslash = "/"))
     }
   }
-} else {
-  # if this is not being run locally ...
-#   mcran <- paste0("file:///",normalizePath("~/Desktop/radiant/radiant-miniCRAN", winslash = "/"))
-  mcran <- "file:///home/vnijs/Desktop/radiant/radiant-miniCRAN"
-}
 
-# look locally first and then in the Rstudion CRAN
-# options(repos = c(CRAN = c(mcran,"http://cran.rstudio.com")))
-options(repos = c(CRAN = mcran))
+  # look locally first and then in the Rstudion CRAN
+  # options(repos = c(CRAN = c(mcran,"http://cran.rstudio.com")))
+  options(repos = c(CRAN = mcran))
 
-# install to user directory
-if(Sys.getenv('SHINY_PORT') == "") {
+  # install to user directory
   local_dir <- Sys.getenv("R_LIBS_USER")
   if(!file.exists(local_dir)) dir.create(local_dir, recursive = TRUE)
 
-# loading the list of pkgs needed to run radiant
-# source("../../radiant-miniCRAN/pkgs.R")
-source(paste0(mcran,"/pkgs.R"))
+  # loading the list of pkgs needed to run radiant
+  source(paste0(mcran,"/pkgs.R"))
 
-# check if all packages in libs are installed locally
-available <- suppressWarnings(sapply(pkgs, require, lib.loc = local_dir, character.only=TRUE))
-inst.libs <- pkgs[available == FALSE]
+  # check if packages in pkgs are alraedy installed locally
+  available <- suppressWarnings(sapply(pkgs, require, lib.loc = local_dir, character.only=TRUE))
+  inst.libs <- pkgs[available == FALSE]
 
-# install and require the packages that have not yet been installed
-if(length(inst.libs) != 0) {
-  install.packages(inst.libs, local_dir)
-  suppressWarnings(sapply(inst.libs, require, lib.loc = local_dir, character.only=TRUE))
-}
+  # install and require the packages that have not yet been installed
+  if(length(inst.libs) != 0) {
+    install.packages(inst.libs, local_dir)
+    suppressWarnings(sapply(inst.libs, require, lib.loc = local_dir, character.only=TRUE))
+  }
 
 } else {
-#   options(repos = c(CRAN = c("http://cran.rstudio.com")))
+  # when run on Shiny-server make sure you have install the packages already
+  # using sudo su
+  mcran <- "file:///home/vnijs/Desktop/radiant/radiant-miniCRAN"
   source(paste0(mcran,"/pkgs.R"))
-#   install.packages(pkgs)
-  suppressWarnings(sapply(pkgs, require, character.only=TRUE))
+  sapply(pkgs, require, character.only=TRUE)
 }
