8439fc0cf890ae49611152c51278a975a560f83b
mostly-harmless <vnijs@ucsd.edu>
2014-09-30 21:10:55 -0700

    Fixing launchers

--- launchers/base/radiant_win_base.bat
+++ launchers/base/radiant_win_base.bat
@@ -4,4 +4,3 @@ set radpath=%radpath:\=/%
 "%R%" -e "Sys.setlocale(category = 'LC_ALL','English_United States.1252'); setwd('%radpath%'); source('radiant-miniCRAN/dependencies.R'); shiny::runApp('inst/base', port = 4000, launch.browser=TRUE)"
 
 pause
-
