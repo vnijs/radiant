2969b717b6a5affd754d2ca238e02d2f822b5454
mostly-harmless <vnijs@ucsd.edu>
2014-10-01 14:24:06 -0700

    Fixing launchers

--- radiant-sync/sync2dropbox.sh
+++ radiant-sync/sync2dropbox.sh
@@ -1,2 +1,3 @@
 #rsync -avruz --delete --exclude-from 'exclude-from-sync2dropbox.txt' ~/Desktop/radiant_dev/ ~/Dropbox/radiant/
 rsync -avruz --exclude-from 'radiant-sync/exclude-from-sync2dropbox.txt' ~/Desktop/radiant_dev/ ~/Dropbox/radiant/
+
