2e9393efe3c5757de6209642c5a279183dd110a9
mostly-harmless <vnijs@ucsd.edu>
2014-10-01 15:01:05 -0700

    Path fix for radiant_dev

--- radiant-sync/sync2dropbox-dry-run.sh
+++ radiant-sync/sync2dropbox-dry-run.sh
@@ -1,4 +1,2 @@
 # check for folder differences without making any changes
 rsync -avruz --dry-run --delete --exclude-from 'radiant-sync/exclude-from-sync2dropbox.txt' ~/Desktop/radiant_dev/ ~/Dropbox/radiant/
-
-# rsync -avruz --dry-run ~/Desktop/radiant_dev_win/launchers/ ~/Desktop/radiant_dev/launchers/
