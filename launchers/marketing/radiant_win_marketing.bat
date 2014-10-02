a37afbd2398eaa0ca38b33ed8c659752fdef2e02
mostly-harmless <vnijs@ucsd.edu>
2014-10-01 21:30:01 -0700

    Update marketing launcher for windows

--- launchers/marketing/radiant_win_marketing.bat
+++ launchers/marketing/radiant_win_marketing.bat
@@ -1,10 +1,3 @@
-set R=C:\Program Files\R\R-3.1.1\bin\x64\R.exe
-set fpath=%APPDATA%/Dropbox/info.json
-set fpath=%fpath:\=/%
-
-"%R%" -e "Sys.setlocale(category = 'LC_ALL','English_United States.1252');  f <- file('%fpath%','r'); json_string <- suppressWarnings(readLines(f, -1L)); json_string"
-path_part <- sub('.*path\\\": \\\"','',json_string); path <- sub('\\\",.*','',path_part); setwd(path); source('radiant-miniCRAN/dependencies.R');  shiny::runApp('inst/marketing', port = 4475, launch.browser=TRUE)"
-
 :: Using 32-bit R unless 64-bit R is available
 set R=C:\Program Files\R\R-3.1.1\bin\R.exe
 IF exist "C:\Program Files\R\R-3.1.1\bin\x64" (set R=C:\Program Files\R\R-3.1.1\bin\x64\R.exe)
