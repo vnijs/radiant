9e9ed9f91bf3209694ef25d59536c4a46e99d391
mostly-harmless <vnijs@ucsd.edu>
2014-10-01 17:06:17 -0700

    Quant launchers update

--- launchers/quant/radiant_win_quant.bat
+++ launchers/quant/radiant_win_quant.bat
@@ -1,8 +1,22 @@
+:: Using 32-bit R unless 64-bit R is available
 set R=C:\Program Files\R\R-3.1.1\bin\R.exe
 IF exist "C:\Program Files\R\R-3.1.1\bin\x64" (set R=C:\Program Files\R\R-3.1.1\bin\x64\R.exe)
 
-set radpath=%HOMEDRIVE%%HOMEPATH%/Dropbox/radiant
-set radpath=%radpath:\=/%
-"%R%" -e "setwd(normalizePath('%radpath%')); Sys.setlocale(category = 'LC_ALL','English_United States.1252'); setwd('%radpath%'); source('radiant-miniCRAN/dependencies.R'); shiny::runApp('inst/quant', port = 4403, launch.browser=TRUE)"
+:: Finding the path to the dropbox info.json file
+set fpath=%APPDATA%/Dropbox/info.json
+set fpath=%fpath:\=/%
+
+:: Commands used
+:: "%R%" -e "Sys.setlocale(category = 'LC_ALL','English_United States.1252');
+:: f <- file('%fpath%','r');
+:: json_string <- suppressWarnings(readLines(f, -1L));
+:: path_part <- sub('.*path\\\": \\\"','',json_string);
+:: path <- paste0(sub('\\\",.*','',path_part), '\\\\radiant');
+:: setwd(normalizePath(path, winslash='/'));
+:: source('radiant-miniCRAN/dependencies.R');
+:: shiny::runApp('inst/marketing', port = 4475, launch.browser=TRUE)"
+
+:: Uglyfied
+"%R%" -e "Sys.setlocale(category = 'LC_ALL','English_United States.1252'); f <- file('%fpath%','r'); json_string <- suppressWarnings(readLines(f, -1L)); path_part <- sub('.*path\\\": \\\"','',json_string); path <- paste0(sub('\\\",.*','',path_part), '\\\\radiant'); setwd(normalizePath(path, winslash='/')); source('radiant-miniCRAN/dependencies.R');  shiny::runApp('inst/quant', port = 4403, launch.browser=TRUE)"
 
 pause
