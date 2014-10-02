f9fcb97ef003f2505044074befc73d28dbdd50cd
mostly-harmless <vnijs@ucsd.edu>
2014-10-01 23:56:57 -0700

    Sync to clean-out radiant on dropbox

--- radiant-sync/sync2dropbox-dry-run.sh
+++ radiant-sync/sync2dropbox-dry-run.sh
@@ -1,2 +1,3 @@
 # check for folder differences without making any changes
-rsync -avruz --dry-run --delete --exclude-from 'radiant-sync/exclude-from-sync2dropbox.txt' ~/Desktop/radiant_dev/ ~/Dropbox/radiant/
+rsync -avruz --dry-run --delete --exclude-from 'radiant-sync/exclude-from-sync2dropbox.txt' ~/Desktop/radiant_dev/ ~/Dropbox/"radiant (1)"/
+
