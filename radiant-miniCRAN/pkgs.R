0602c0178e6f9e5f722e167d04aa6f9a6bf6201d
mostly-harmless <vnijs@ucsd.edu>
2014-10-02 15:22:59 -0700

    Add rvest packages when on CRAN

--- radiant-miniCRAN/pkgs.R
+++ radiant-miniCRAN/pkgs.R
@@ -3,3 +3,7 @@ pkgs <- c("mime","BH","GPArotation","R.methodsS3", "R.oo", "R.utils", "car",
 	"gridExtra", "psych", "vegan", "lubridate", "RColorBrewer",
 	"wordcloud", "AlgDesign", "markdown", "knitr", "plyr", "tidyr",
 	"dplyr", "ggplot2", "shiny", "shinyAce")
+
+# Add rvest when it gets to CRAN
+# https://github.com/hadley/rvest
+
