1b395b14aef37cdbad603bd530941f17bb9e1eab
mostly-harmless <vnijs@ucsd.edu>
2014-10-01 21:07:52 -0700

    Fixing launchers for Quant, Marketing, and Base

--- launchers/base/radiant_win_base.bat
+++ launchers/base/radiant_win_base.bat
@@ -1,6 +1,22 @@
-set R=C:\Program Files\R\R-3.1.1\bin\x64\R.exe
-set radpath=%HOMEDRIVE%%HOMEPATH%/Dropbox/radiant
-set radpath=%radpath:\=/%
-"%R%" -e "Sys.setlocale(category = 'LC_ALL','English_United States.1252'); setwd('%radpath%'); source('radiant-miniCRAN/dependencies.R'); shiny::runApp('inst/base', port = 4000, launch.browser=TRUE)"
+:: Using 32-bit R unless 64-bit R is available
+set R=C:\Program Files\R\R-3.1.1\bin\R.exe
+IF exist "C:\Program Files\R\R-3.1.1\bin\x64" (set R=C:\Program Files\R\R-3.1.1\bin\x64\R.exe)
+
+:: Finding the path to the dropbox info.json file
+set fpath=%APPDATA%/Dropbox/info.json
+set fpath=%fpath:\=/%
+
+:: Commands used
+:: "%R%" -e "Sys.setlocale(category = 'LC_ALL','English_United States.1252');
+:: f <- file('%fpath%','r');
+:: json_string <- suppressWarnings(readLines(f, -1L));
+:: path_part <- sub('.*path\\\": \\\"','',json_string);
+:: path <- paste0(sub('\\\",.*','',path_part), '\\\\radiant');
+:: setwd(normalizePath(path, winslash='/'));
+:: source('radiant-miniCRAN/dependencies.R');
+:: shiny::runApp('inst/base', port = 4000, launch.browser=TRUE)"
+
+:: Uglyfied
+"%R%" -e "Sys.setlocale(category = 'LC_ALL','English_United States.1252'); f <- file('%fpath%','r'); json_string <- suppressWarnings(readLines(f, -1L)); path_part <- sub('.*path\\\": \\\"','',json_string); path <- paste0(sub('\\\",.*','',path_part), '\\\\radiant'); setwd(normalizePath(path, winslash='/')); source('radiant-miniCRAN/dependencies.R');  shiny::runApp('inst/base', port = 4000, launch.browser=TRUE)"
 
 pause
--- launchers/quant/radiant_win_quant.bat
+++ launchers/quant/radiant_win_quant.bat
@@ -14,7 +14,7 @@ set fpath=%fpath:\=/%
 :: path <- paste0(sub('\\\",.*','',path_part), '\\\\radiant');
 :: setwd(normalizePath(path, winslash='/'));
 :: source('radiant-miniCRAN/dependencies.R');
-:: shiny::runApp('inst/marketing', port = 4475, launch.browser=TRUE)"
+:: shiny::runApp('inst/quant', port = 4403, launch.browser=TRUE)"
 
 :: Uglyfied
 "%R%" -e "Sys.setlocale(category = 'LC_ALL','English_United States.1252'); f <- file('%fpath%','r'); json_string <- suppressWarnings(readLines(f, -1L)); path_part <- sub('.*path\\\": \\\"','',json_string); path <- paste0(sub('\\\",.*','',path_part), '\\\\radiant'); setwd(normalizePath(path, winslash='/')); source('radiant-miniCRAN/dependencies.R');  shiny::runApp('inst/quant', port = 4403, launch.browser=TRUE)"
