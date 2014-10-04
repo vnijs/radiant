2f1d656a5b23fcae3ae647985a31cc27aa8e2465
mostly-harmless <vnijs@ucsd.edu>
2014-10-04 03:07:25 -0700

    Updated miniCRAN setup

---
+++ radiant-miniCRAN/build_from_github.R
@@ -0,0 +1,10 @@
+# to install the update to Shiny use the below
+pkgs <- c("shiny","shinyAce","rvest","tidyr","dplyr")
+
+# building packages using devtools
+# library(devtools)
+# build('shiny')
+# build('shiny', binary = TRUE)
+
+# build for windows
+# build_win(pkg = ".", version = c("R-release", "R-devel"), args = NULL, quiet = FALSE)
--- radiant-miniCRAN/pkgs.R
+++ radiant-miniCRAN/pkgs.R
@@ -1,9 +1,8 @@
 # Specify list of packages needed
-pkgs <- c("mime","BH","GPArotation","R.methodsS3", "R.oo", "R.utils", "car",
+pkgs_cran <- c("mime","BH","GPArotation","R.methodsS3", "R.oo", "R.utils", "car",
 	"gridExtra", "psych", "vegan", "lubridate", "RColorBrewer",
-	"wordcloud", "AlgDesign", "markdown", "knitr", "plyr", "tidyr",
-	"dplyr", "ggplot2", "shiny", "shinyAce")
+	"wordcloud", "AlgDesign", "markdown", "rmarkdown", "knitr", "plyr", "ggplot2")
 
-# Add rvest when it gets to CRAN
-# https://github.com/hadley/rvest
+pkgs_gh <- c("shiny","shinyAce","rvest","tidyr","dplyr")
 
+pkgs <- c(pkgs_cran,pkgs_gh)
