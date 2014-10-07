6285f712b0480d496f05d12f2ca0c35cef2b0e89
mostly-harmless <vnijs@ucsd.edu>
2014-10-07 03:28:34 -0700

    Upatedocs and ..

--- inst/base/server.R
+++ inst/base/server.R
@@ -6,4 +6,15 @@ shinyServer(function(input, output, session) {
 	# source data & analysis tools
 	R.utils::sourceDirectory('tools/data', recursive = TRUE, modifiedOnly = FALSE)
 	R.utils::sourceDirectory('tools/app', recursive = TRUE, modifiedOnly = FALSE)
+
+# 	if (grepl("\\W", input$partial))
+# 	  return(NULL)
+#
+	# Source the partials/<pageTab>.R file and return the result.
+	# It's important to pass local=TRUE so evaluation happens here,
+	# not in the global environment.
+# 	source(file.path("tools/data"), local=TRUE)
+# 	list.files(file.path("tools/data"), full.names = TRUE)
+# ?list.files
+
 })
