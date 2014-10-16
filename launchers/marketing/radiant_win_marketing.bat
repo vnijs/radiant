274cef41359aaf3035da304ac12cc404730fc048
mostly-harmless <vnijs@ucsd.edu>
2014-10-16 01:30:42 -0700

    Windows line-ending in .bat files

--- launchers/marketing/radiant_win_marketing.bat
+++ launchers/marketing/radiant_win_marketing.bat
@@ -20,4 +20,4 @@ set fpath=%fpath:\=/%
 :: Uglyfied
 "%R%" -e "Sys.setlocale(category = 'LC_ALL','English_United States.1252'); f <- file('%fpath%','r'); json_string <- suppressWarnings(readLines(f, -1L)); path_part <- sub('.*path\\\": \\\"','',json_string); close(f); path <- paste0(sub('\\\",.*','',path_part), '\\\\radiant'); setwd(normalizePath(path, winslash='/')); source('radiant-miniCRAN/dependencies.R');  shiny::runApp('inst/marketing', port = 4475, launch.browser=TRUE)"
 
-pause
+:: pause
