cba2c4e1da906052e3fd1cdd1c3cacf25e6691c1
mostly-harmless <vnijs@ucsd.edu>
2014-10-18 11:49:55 -0700

    Create launchers when shiny is not yet installed

--- launchers/createLauncher/batCreator.R
+++ launchers/createLauncher/batCreator.R
@@ -1,6 +1,19 @@
 # commands to run Radiant
 args <- commandArgs(trailingOnly = TRUE)
 apppth <- "radiant/inst/quant"
+
+
+if(!'shiny' %in% installed.packages()[,'Package']) {
+  # setting the url for the miniCRAN
+  local_dir <- Sys.getenv("R_LIBS_USER")
+  if(!file.exists(local_dir)) dir.create(local_dir, recursive = TRUE)
+#   pth <- normalizePath(getwd())
+  pth <- normalizePath(args)
+  mcran <- paste0("file:///",pth)
+  options(repos = c(CRAN = mcran))
+  update.packages(lib.loc = local_dir, ask = FALSE)
+}
+
 com_string <- paste0("cd ",args,"\n",Sys.which('R'), " -e ",
 	"\"shiny::runApp('",apppth,"',port = 4403, launch.browser=TRUE)\"\npause")
 
