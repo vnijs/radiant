5190e45b66b44e6733983207f3603d21c21efcb9
mostly-harmless <vnijs@ucsd.edu>
2014-10-25 21:03:28 -0700

    Fixin-fixin launchers

---
+++ launchers/make/make_mac_alt.R
@@ -0,0 +1,29 @@
+class_app <- commandArgs(trailingOnly = TRUE)
+
+R_loc <- Sys.which('R')
+if(R_loc == "") {
+  cat("R is not installed or is not installed in the expected location")
+  cat("\nMake sure R is installed on your system\n")
+  Sys.sleep(60)
+} else {
+  cat("R was found in ",R_loc,"\n")
+  script_dir <- getwd()
+
+  instpack <- installed.packages()[,'Package']
+  if(!'shiny' %in% instpack || !'httpuv' %in% instpack || !'shinyAce' %in% instpack) {
+    # setting the url for the miniCRAN
+    local_dir <- Sys.getenv("R_LIBS_USER")
+    if(!file.exists(local_dir)) dir.create(local_dir, recursive = TRUE)
+    pth <- normalizePath(paste0(script_dir,'/../../radiant-miniCRAN'),winslash='/')
+    mcran <- paste0("file:///",pth)
+    options(repos = c(CRAN = mcran))
+    install.packages('shiny', local_dir, dependencies = TRUE)
+    install.packages('shinyAce', local_dir, dependencies = TRUE)
+  }
+
+  app_dir <- normalizePath(paste0(script_dir,"/../../inst/",class_app[1],"/"))
+  com_string <- paste0("#!/usr/bin/env Rscript\nshiny::runApp('",app_dir,"', port = ",class_app[2],", launch.browser=TRUE)")
+  filename <- paste0("/Users/",Sys.getenv("USER"),"/Desktop/radiant_",class_app[1],"_alt.command")
+  cat(com_string,file=filename,sep="\n")
+  Sys.chmod(filename, mode = "0755")
+}
--- radiant-sync/sync2dropbox.sh
+++ radiant-sync/sync2dropbox.sh
@@ -1,5 +1,5 @@
 ### Copying over the main code files
-#   rsync --dry-run -avruz --exclude-from 'radiant-sync/exclude-from-sync2dropbox.txt' ~/Desktop/GitHub/radiant_dev/ ~/Dropbox/radiant/
+#  rsync --dry-run -avruz --exclude-from 'radiant-sync/exclude-from-sync2dropbox.txt' ~/Desktop/GitHub/radiant_dev/ ~/Dropbox/radiant/
 #  rsync -avruz --exclude-from 'radiant-sync/exclude-from-sync2dropbox.txt' ~/Desktop/GitHub/radiant_dev/ ~/Dropbox/radiant/
 
 ### Cleaning out stuff that is no longer needed
