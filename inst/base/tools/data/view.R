4a6854b7096b8bdef005f03305b790563ca68d6e
mostly-harmless <vnijs@ucsd.edu>
2014-10-29 10:52:09 -0700

    Improved filter and error message

--- inst/base/tools/data/view.R
+++ inst/base/tools/data/view.R
@@ -8,15 +8,8 @@ output$uiView_vars <- renderUI({
 })
 
 output$uiView_subsbig <- renderUI({
-#   view_select <- ""
-#   if(!is.null(input$view_select) && input$view_select != "") {
-#     view_select <- stringr::str_trim(input$view_select)
-#   }
-
   list(
-#     tags$textarea(id="view_subsbig_area", rows=3, cols=5, "", state_init("view_subsbig_area",input$view_select)),
     tags$textarea(id="view_subsbig_area", rows=3, cols=5, "", state_init("view_subsbig_area",isolate(input$view_select))),
-#     tags$textarea(id="view_subsbig_area", class="myclass", rows=3, cols=5, "", state_init("view_subsbig_area",view_select)),
     actionButton("view_subsbig_action", "Submit command")
   )
 })
@@ -33,22 +26,14 @@ output$ui_View <- renderUI({
   list(
     wellPanel(
       uiOutput("uiView_vars"),
-#       actionLink("view_alink", "Subset (e.g., price > 5000)"),
       checkboxInput('view_subsbig', 'Subset (price > 5000)', FALSE),
-#       returnTextInput("view_select", "Subset (e.g., price > 5000)", state_init("view_select"))
       conditionalPanel(condition = "input.view_subsbig == false",
          returnTextInput("view_select", "", state_init("view_select"))
-#          returnTextInput("view_select", "", state_init("view_select",isolate(input$view_subsbigarea)))
       ),
       conditionalPanel(condition = "input.view_subsbig == true",
         uiOutput("uiView_subsbig")
       ),
-#        conditionalPanel(condition = "output.uiView_err != null",
-#       conditionalPanel(condition = "values.error != ''",
-#                        helpText(isolate(values$error))
-                        uiOutput("uiView_err")
-#                        helpText(values$error)
-#       )
+      uiOutput("uiView_err")
     ),
     helpModal('View','viewHelp',inclMD("../base/tools/help/view.md"))
   )
@@ -66,84 +51,32 @@ output$dataviewer <- renderDataTable({
 
   # if(isolate(input$datatabs) != 'View') return(invisible())
   # if(is.null(input$datasets) || is.null(input$view_vars)) return()
-
   if(is.null(input$view_vars)) return()
   # dat <- date2character()
   dat <- getdata()
 
   if(!all(input$view_vars %in% colnames(dat))) return()
 
-#   input$view_select
-#   input$view_subsbig
-#
-#   selcom <-
-#   selcom <- input$view_select
-# dataDescriptionOutput
-#         selcom <- isolate(values[[paste0(input$datasets,"_filter")]])
+  selcom <- gsub("\\s","",input$view_select)
+  if(!(is.null(input$view_subsbig_action) || input$view_subsbig_action == 0)) {
+    isolate({
 
-
-#     x <- "
-# "
-# gsub("\\s","",x)
-
-    selcom <- gsub("\\s","",input$view_select)
-    if(!(is.null(input$view_subsbig_action) || input$view_subsbig_action == 0)) {
-      isolate({
-
-        if(input$view_subsbig) {
-          view_subsbig_area <- gsub("\\s", "", input$view_subsbig_area)
-          if(view_subsbig_area != "") {
-            selcom <- view_subsbig_area
-   	        updateTextInput(session = session, "view_select", "", stringr::str_trim(input$view_subsbig_area))
-          }
-        } else {
-          updateTextInput(session = session, "view_subsbig_area", "", input$view_select)
+      if(input$view_subsbig) {
+        view_subsbig_area <- gsub("\\s", "", input$view_subsbig_area)
+        if(view_subsbig_area != "") {
+          selcom <- view_subsbig_area
+ 	        updateTextInput(session = session, "view_select", "", stringr::str_trim(input$view_subsbig_area))
         }
-      })
-    } else {
-#  	 	  updateTextInput(session = session, "view_subsbig_area", "", input$view_select)
-# 	 	  updateTextInput(session = session, "view_subsbig_area", "", "")
-
-# 	 	  updateTextInput(session = session, "view_subsbig_area", "", input$view_select)
-#  	 	  if(!input$view_subsbig) updateTextInput(session = session, "view_subsbig_area", "","")
-# 	 	  if(selcom != "") updateTextInput(session = session, "view_subsbig_area", "","")
-    }
-
-#     if(!input$view_subsbig && input$view_subsbig_area != "") updateTextInput(session = session, "view_subsbig_area", "","")
-#     if(!input$view_subsbig && input$view_select != "") updateTextInput(session = session, "view_subsbig_area", "","")
-#     if(!input$view_subsbig && selcom != "") updateTextInput(session = session, "view_subsbig_area", "","")
-
-#     if(input$view_subsbig ) || input$view_subsbig_action == 0)) {
-# 	 	      updateTextInput(session = session, "view_select", "","")
-#   selcom = ""
-#   if(input$view_select == "") {
-#     if(is.null(input$view_subsbig_action) || input$view_subsbig_action == 0) {
-#       selcom = ""
-#     } else {
-#       isolate({
-#         if(input$view_subsbig_area != "") selcom <- input$view_subsbig_area
-# #         selcom <- isolate(values[[paste0(input$datasets,"_filter")]])
-#       })
-#     }
-#   } else {
-#     selcom <- input$view_select
-#   }
-
-  print(selcom)
+      } else {
+        updateTextInput(session = session, "view_subsbig_area", "", input$view_select)
+      }
+    })
+  } else {
+    updateTextInput(session = session, "view_subsbig_area", "", input$view_select)
+  }
 
   selcom <- gsub("\\s", "", selcom)
   if(selcom != "") {
-#     seldat <- try(do.call(subset, list(dat,parse(text = selcom))), silent = TRUE)
-#     filter_(dat, "cyl == 8")
-
-#     seldat <- try(filter_(list(dat,parse(text = selcom))), silent = TRUE)
-
-#     dat <- mtcars
-#     selcom <- "mpg > 27x"
-#     selcom <- "ook"
-#     seldat <- try(filter_(dat, selcom))
-
-
     seldat <- try(filter_(dat, selcom), silent = TRUE)
     if(!is(seldat, 'try-error')) {
       values$error <- ""
@@ -152,7 +85,6 @@ output$dataviewer <- renderDataTable({
         seldat <- NULL
       }
     } else {
-#       cat(attr(seldat,"condition")$message)
       isolate(values$error <- attr(seldat,"condition")$message)
     }
   } else {
@@ -164,7 +96,3 @@ output$dataviewer <- renderDataTable({
 }, options = list(orderClasses = TRUE, caseInsensitive = TRUE,
   lengthMenu = list(c(10, 25, 50, -1),c('10','25','50','All')),
   pageLength = 10, search = list(regex = TRUE)))
-
-# }, options = list(bSortClasses = TRUE, bCaseInsensitive = TRUE,
-#   lengthMenu = list(c(10, 25, 50, -1),c('10','25','50','All')),
-#   pageLength = 10, search = list(regex = TRUE)))
