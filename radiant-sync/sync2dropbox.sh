076fa28aec624e938167c48870a8342bc94a28ad
mostly-harmless <vnijs@ucsd.edu>
2014-11-08 01:20:38 -0800

    Cleanup and sync

--- radiant-sync/sync2dropbox.sh
+++ radiant-sync/sync2dropbox.sh
@@ -6,6 +6,7 @@
 # rsync --dry-run -avruz --exclude-from 'radiant-sync/exclude-from-sync2dropbox.txt' ~/Desktop/GitHub/radiant_dev/ ~/Dropbox/radiant/
 # rsync -avruz --exclude-from 'radiant-sync/exclude-from-sync2dropbox.txt' ~/Desktop/GitHub/radiant_dev/ ~/Dropbox/radiant/
 
+
 ### Cleaning out stuff that is no longer needed
 # rsync --dry-run -avruz --delete ~/Desktop/GitHub/radiant_dev/launchers/ ~/Dropbox/radiant/launchers/
 # rsync -avruz --delete ~/Desktop/GitHub/radiant_dev/launchers/ ~/Dropbox/radiant/launchers/
@@ -16,6 +17,11 @@
 # rsync --dry-run -avruzm -f'+ PACKAGE*' -f'+ */' -f'- *' ~/Desktop/GitHub/radiant_dev/radiant-miniCRAN/bin/ ~/Dropbox/radiant/radiant-miniCRAN/bin/
 # rsync -avruzm -f'+ PACKAGE*' -f'+ */' -f'- *' ~/Desktop/GitHub/radiant_dev/radiant-miniCRAN/bin/ ~/Dropbox/radiant/radiant-miniCRAN/bin/
 
+# diff -x "*.gz" -Nr ~/Desktop/GitHub/radiant_dev/radiant-miniCRAN/bin/ ~/Dropbox/radiant/radiant-miniCRAN/bin/
+# diff -Nr ~/Desktop/GitHub/radiant_dev/radiant-miniCRAN/bin/windows/contrib/3.1/PACKAGES ~/Dropbox/radiant/radiant-miniCRAN/bin/windows/contrib/3.1/PACKAGES
+# diff -Nr ~/Desktop/GitHub/radiant_dev/radiant-miniCRAN/bin/macosx/contrib/3.1/PACKAGES ~/Dropbox/radiant/radiant-miniCRAN/bin/macosx/contrib/3.1/PACKAGES
+# diff -x ".DS_Store" -Naur ~/Desktop/GitHub/radiant_dev/launchers/ ~/Dropbox/radiant/launchers/
+
 # only copy the files that do not exist to dropbox
 # rsync --dry-run -avrz --delete --ignore-existing ~/Desktop/GitHub/radiant_dev/radiant-miniCRAN/ ~/Dropbox/radiant/radiant-miniCRAN/
 # rsync -avrz --delete --ignore-existing ~/Desktop/GitHub/radiant_dev/radiant-miniCRAN/ ~/Dropbox/radiant/radiant-miniCRAN/
@@ -23,5 +29,18 @@
 # rsync --dry-run -avruz --delete ~/Desktop/GitHub/radiant_dev/dev/base/ ~/Dropbox/radiant/dev/base/
 # rsync -avruz --delete ~/Desktop/GitHub/radiant_dev/dev/base/ ~/Dropbox/radiant/dev/base/
 
+# syncing the dev branch
+# rsync --dry-run -avruz --delete ~/Desktop/GitHub/radiant_dev/dev/base/ ~/Dropbox/radiant/dev/base/
 # rsync --dry-run -avruz --delete ~/Desktop/GitHub/radiant_dev/dev/quant/ ~/Dropbox/radiant/dev/quant/
 # rsync --dry-run -avruz --delete ~/Desktop/GitHub/radiant_dev/dev/marketing/ ~/Dropbox/radiant/dev/marketing/
+
+# rsync -avruz --delete ~/Desktop/GitHub/radiant_dev/dev/base/ ~/Dropbox/radiant/dev/base/
+# rsync -avruz --delete ~/Desktop/GitHub/radiant_dev/dev/quant/ ~/Dropbox/radiant/dev/quant/
+# rsync -avruz --delete ~/Desktop/GitHub/radiant_dev/dev/marketing/ ~/Dropbox/radiant/dev/marketing/
+
+# diff -x ".DS_Store" -Naur ~/Dropbox/radiant/inst/ ~/Desktop/GitHub/radiant_dev/inst/
+# diff -x ".DS_Store" -Naur ~/Dropbox/radiant/inst/ ~/Desktop/GitHub/radiant_dev/dev/
+
+diff -x ".DS_Store" -x "*.rda" -Naur ~/Dropbox/radiant/inst/base/ ~/Desktop/GitHub/radiant_dev/dev/base/
+
+
