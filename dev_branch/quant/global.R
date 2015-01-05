9bd23523442a8bc08e8f80d48af8d4ef4eed676d
mostly-harmless <vnijs@ucsd.edu>
2015-01-05 14:14:45 -0800

    removing the dev_branch directory - from now on use add_dev to develop and test components

--- dev_branch/quant/global.R
+++
@@ -1,5 +0,0 @@
-# sourcing from radiant base
-source('../base/global.R', local = TRUE)
-
-# add functions below that may not be needed in other apps
-addResourcePath("figures_quant", "../quant/tools/help/figures/")
