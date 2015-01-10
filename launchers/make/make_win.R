8a71262a5c82190e83eec6493d7f5564e2e96a08
mostly-harmless <vnijs@ucsd.edu>
2015-01-09 18:11:04 -0800

    Using vnijs.rady.ucsd.edu as miniCRAN

--- launchers/make/make_win.R
+++ launchers/make/make_win.R
@@ -7,12 +7,23 @@ class_app <- args[2:3]
 local_dir <- Sys.getenv("R_LIBS_USER")
 if(!file.exists(local_dir)) dir.create(local_dir, recursive = TRUE)
 pth <- normalizePath(paste0(script_dir,'/../../miniCRAN'),winslash='/')
-mcran <- paste0("file:///",pth)
-options(repos = c(CRAN = mcran))
+# mcran <- paste0("file:///",pth)
+# options(repos = c(CRAN = mcran))
+options(repos = "http://vnijs.rady.ucsd.edu/site_media/R")
 
 # install.packages('shiny', local_dir, dependencies = TRUE)
 source(paste0(pth,"/pkgs.R"))
-install.packages(pkgs, local_dir, dependencies = TRUE)
+
+# udpate old-packages
+update.packages(lib.loc = local_dir, ask = FALSE)
+
+# install packages that are available but were not installed before
+to_inp <- new.packages(lib.loc = local_dir)
+if(length(to_inp) != 0)
+	install.packages(to_inp, local_dir, dependencies = TRUE)
+
+# install from scratch no matter if any packages are available
+# install.packages(pkgs, local_dir, dependencies = TRUE)
 
 com_string <- paste0("cd ",script_dir,"\n",Sys.which('R'), " -e ",
   "\"shiny::runApp('../../inst/", class_app[1],"', port = ", class_app[2],", launch.browser=TRUE)\"")
