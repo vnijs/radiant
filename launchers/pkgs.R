3bfeaa1266e031ec26e2227e0afc6fcf1f43a208
mostly-harmless <vnijs@ucsd.edu>
2015-01-14 12:23:53 -0800

    Using github as the miniCRAN repo

---
+++ launchers/pkgs.R
@@ -0,0 +1,6 @@
+# list of pkgs needed to run radiant
+pkgs <- c("car", "gridExtra", "GPArotation", "psych", "vegan",
+          "RColorBrewer", "wordcloud", "AlgDesign", "brew",
+          "reshape2", "plyr", "markdown", "knitr", "rmarkdown",
+          "testthat", "lubridate", "ggplot2", "shiny","magrittr",
+          "tidyr", "dplyr", "ggvis", "broom", "shinyAce")
