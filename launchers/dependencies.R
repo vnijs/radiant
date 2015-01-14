81fd8f7ef90d4b50ceb5b435b7dbbdc436d6bbbe
mostly-harmless <vnijs@ucsd.edu>
2015-01-14 12:14:51 -0800

    Use github repo (3.1.2)

--- launchers/dependencies.R
+++ launchers/dependencies.R
@@ -13,14 +13,23 @@ pth <- normalizePath("../../launchers", winslash = "/")
 local_dir <- Sys.getenv("R_LIBS_USER")
 if(!file.exists(local_dir)) dir.create(local_dir, recursive = TRUE)
 
-# loading the list of pkgs needed to run radiant
-source(paste0(pth,"/pkgs.R"))
 
-# setting the url for the miniCRAN
-# look to rady server first and then in the Rstudio CRAN
-# mcran <- paste0("file:///",pth)
-# options(repos = c(CRAN = "http://vnijs.rady.ucsd.edu/site_media/R"))
-options(repos = "http://vnijs.rady.ucsd.edu/site_media/R")
+# setting the url for the miniCRAN, using XRAN as in yihui/xran :)
+# options(repos = c(XRAN="http://vnijs.rady.ucsd.edu/site_media/R", CRAN="http://cran.rstudio.com"))
+# options(repos = "http://vnijs.rady.ucsd.edu/site_media/R")
+options(repos = c(XRAN = "http://mostly-harmless.github.io/radiant_miniCRAN/"))
+
+# preferable to use online list BUT the import order is important
+# (e.g., plyr and dplyr)
+# pkgs <- available.packages() %>% rownames
+# installed.packages() %>% rownames
+
+# list of pkgs needed to run radiant
+pkgs <- c("car", "gridExtra", "GPArotation", "psych", "vegan",
+          "RColorBrewer", "wordcloud", "AlgDesign", "brew",
+          "reshape2", "plyr", "markdown", "knitr", "rmarkdown",
+          "testthat", "lubridate", "ggplot2", "shiny","magrittr",
+          "tidyr", "dplyr", "ggvis", "broom", "shinyAce")
 
 # udpate old-packages
 update.packages(lib.loc = local_dir, ask = FALSE)
@@ -29,10 +38,6 @@ update.packages(lib.loc = local_dir, ask = FALSE)
 to_inp <- new.packages(lib.loc = local_dir)
 if(length(to_inp) != 0)
 	install.packages(to_inp, local_dir, dependencies = TRUE)
-
-# add function to remove packages that are not used by Radiant?
-# might mess up someones stuff. maybe better to do this like packRat does it
-# i.e., install into a non-standard directory
 
 # load/attach packages
 suppressWarnings(
