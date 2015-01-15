b9b93535fef8a6546dd4aaf812d7546b12367729
mostly-harmless <vnijs@ucsd.edu>
2015-01-15 11:21:34 -0800

    Remove class to files outside of inst/

--- launchers/pkgs.R
+++
@@ -1,6 +0,0 @@
-# list of pkgs needed to run radiant
-pkgs <- c("car", "gridExtra", "GPArotation", "psych", "vegan",
-          "RColorBrewer", "wordcloud", "AlgDesign", "brew",
-          "reshape2", "plyr", "markdown", "knitr", "rmarkdown",
-          "testthat", "lubridate", "ggplot2", "shiny","magrittr",
-          "tidyr", "dplyr", "ggvis", "broom", "shinyAce")
