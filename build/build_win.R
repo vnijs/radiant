1ce8348d5b96cb6be8bad70e94d18617ab02159a
Vincent Nijs <vnijs@ucsd.edu>
2015-01-21 17:14:42 -0800

    Windows update

--- build/build_win.R
+++ build/build_win.R
@@ -1,3 +1,12 @@
 # run this separately on mac and windows
+# install.packages('devtools')
 library(devtools)
-build('radiant', binary = TRUE)
+document(roclets=c('rd', 'collate', 'namespace'))
+build('../radiant_dev', binary = TRUE)
+
+setwd('../')
+rfile <- Sys.glob("radiant*zip")
+setwd('radiant_dev')
+
+file.copy(paste0("../",rfile),"Z:/Desktop/GitHub")
+file.remove(paste0("../",rfile))
--- build/build_win.bat
+++
@@ -1,4 +0,0 @@
-R --save < build_win.R 2>&1
-
-del ../radiant_miniCRAN/bin/windows/contrib/3.1/radiant*
-mv ../*.zip ../radiant_miniCRAN/bin/windows/contrib/3.1/
