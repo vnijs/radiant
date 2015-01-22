5aad6e6e9cbdb70b1949570a1f1bdfd791247454
mostly-harmless <vnijs@ucsd.edu>
2015-01-21 16:56:05 -0800

    Build tools

---
+++ build/write_package_files.R
@@ -0,0 +1,10 @@
+library(tools)
+write_PACKAGES('../radiant_miniCRAN/bin/macosx/contrib/3.1/',type = 'mac.binary')
+write_PACKAGES('../radiant_miniCRAN/bin/macosx/mavericks/contrib/3.1/',type = 'mac.binary')
+write_PACKAGES('../radiant_miniCRAN/bin/windows/contrib/3.1/',type = 'win.binary')
+write_PACKAGES('../radiant_miniCRAN/src/contrib/',type = 'source')
+
+# dirsource=../radiant_miniCRAN/src/contrib/
+# dirmac=../radiant_miniCRAN/bin/macosx/contrib/3.1/
+# dirmac_mav=../radiant_miniCRAN/bin/macosx/mavericks/contrib/3.1/
+# dirwin=../radiant_miniCRAN/bin/windows/contrib/3.1/
