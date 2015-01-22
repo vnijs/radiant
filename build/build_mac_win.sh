4a99e40278fab10dd60513f4d2e8d6d9dbbf25d0
mostly-harmless <vnijs@ucsd.edu>
2015-01-21 16:58:26 -0800

    Package tools

--- build/build_mac_win.sh
+++ build/build_mac_win.sh
@@ -10,17 +10,10 @@ dirwin=../radiant_miniCRAN/bin/windows/contrib/3.1/
 # mv ../*.tar.gz $dirsource
 # cp ../*.tgz $dirmac
 # mv ../*.tgz $dirmac_mav
-
-mv ../*.zip $dirwin
-
-# library(tools)
-# write_PACKAGES('../radiant_miniCRAN/bin/macosx/contrib/3.1/',type = 'mac.binary')
-# write_PACKAGES('../radiant_miniCRAN/bin/macosx/mavericks/contrib/3.1/',type = 'mac.binary')
-# write_PACKAGES('../radiant_miniCRAN/bin/windows/contrib/3.1/',type = 'win.binary')
-# write_PACKAGES('../radiant_miniCRAN/src/contrib/',type = 'source')
+# mv ../*.zip $dirwin
 
 #
-# R --save < writePackages.R 2>&1
+# R --save < write_package_files.R 2>&1
 # cd ../radiant_miniCRAN
 # git push
 # cd ../radiant_dev
