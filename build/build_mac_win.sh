f7c77e179fcec5a6f94c0e9a39cb115e29a73bd8
mostly-harmless <vnijs@ucsd.edu>
2015-01-23 23:44:32 -0800

    Update description

--- build/build_mac_win.sh
+++ build/build_mac_win.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-cd ~/Desktop/GitHub/radiant_dev
+# cd ~/Desktop/GitHub/radiant_dev
+#
+# dirsource=../radiant_miniCRAN/src/contrib/
+# dirmac=../radiant_miniCRAN/bin/macosx/contrib/3.1/
+# dirmac_mav=../radiant_miniCRAN/bin/macosx/mavericks/contrib/3.1/
+# dirwin=../radiant_miniCRAN/bin/windows/contrib/3.1/
+#
+# R --save < build/build_mac_source.R 2>&1
+# mv ../*.tar.gz $dirsource
+# cp ../*.tgz $dirmac
+# mv ../*.tgz $dirmac_mav
+# mv ../*.zip $dirwin
 
-dirsource=../radiant_miniCRAN/src/contrib/
-dirmac=../radiant_miniCRAN/bin/macosx/contrib/3.1/
-dirmac_mav=../radiant_miniCRAN/bin/macosx/mavericks/contrib/3.1/
-dirwin=../radiant_miniCRAN/bin/windows/contrib/3.1/
-
-R --save < build/build_mac_source.R 2>&1
-mv ../*.tar.gz $dirsource
-cp ../*.tgz $dirmac
-mv ../*.tgz $dirmac_mav
-mv ../*.zip $dirwin
-
-# R --save < write_package_files.R 2>&1
+R --save < build/write_package_files.R 2>&1
 # cd ../radiant_miniCRAN
+# git add .
 # git commit -m "radiant package updates"
 # git push
 # cd ../radiant_dev
