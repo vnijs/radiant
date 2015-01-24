d2ab92bf739827e221d0a5fb16fb8f0f1631f13d
mostly-harmless <vnijs@ucsd.edu>
2015-01-23 16:45:08 -0800

    functions for managing files and state (email on state delete)

--- inst/base/init.R
+++ inst/base/init.R
@@ -30,7 +30,69 @@ init_state <- function(values) {
 
 ip_inputs <- paste0("RadiantInputs",ip)
 ip_values <- paste0("RadiantValues",ip)
+ip_dump <- paste0("RadiantDumpTime",ip)
 
+#### test section
+# rm(list = ls())
+# library(lubridate)
+# library(dplyr)
+# t1 <- now()
+# t2 <- t1 + 30
+# less_1 <- t2 - t1
+# t2 <- t1 + minutes(1)
+# less_2 <- t2 - t1
+# t2 <- t1 + minutes(10)
+# more_2 <- t2 - t1
+# str(less_2)
+# attr(less_2,"units")
+
+# ip <- "127.0.0.1"
+# ip_dump <- paste0("RadiantDumpTime",ip)
+# assign(ip_dump, less_3)
+
+# ip <- "127.0.0.2"
+# ip_dump2 <- paste0("RadiantDumpTime",ip)
+# assign(ip_dump2, more_2)
+
+# email may work only on linux
+# check_state_dump_times()
+#### end test section
+
+state_email <- function(p1, p2) {
+  if(!require(sendmailR))
+    install.packages("sendmailR", repos = "http://cran.rstudio.com")
+  library(sendmailR)
+
+  from <- '<vincent.nijs@gmail.com>'
+  to <- '<vincent.nijs@gmail.com>'
+  subject <- 'State of state files'
+  body <- list(p1,"\n\n",p2)
+  sendmail(from, to, subject, body,
+           control=list(smtpServer='ASPMX.L.GOOGLE.COM'))
+}
+
+check_state_dump_times <- function() {
+  dump_times <- ls(pattern = "^RadiantDumpTime", envir = .GlobalEnv)
+  for(i in dump_times) {
+    dump_time <- get(i)
+    dump_time
+    if (attr(dump_time, "units") != "secs" && dump_time > 2) {
+      body_part1 <- c("Before:",ls(pattern="^Radiant" ,envir = .GlobalEnv))
+      sub("RadiantDumpTime","",i) %>%
+        paste0(c("RadiantInputs","RadiantValues","RadiantDumpTime"),.) %>%
+        rm(list = ., envir = .GlobalEnv)
+      body_part2 <- c("After:",ls(pattern="^Radiant" ,envir = .GlobalEnv))
+      state_email(body_part1,body_part2)
+    }
+  }
+}
+
+if(!running_local) {
+  # are there any state files dumped more than 3 minutes ago?
+  check_state_dump_times()
+}
+
+# load previous state if available
 if (exists("state_list") && exists("values")) {
   values <- do.call(reactiveValues, values)
   state_list <- state_list
@@ -38,16 +100,9 @@ if (exists("state_list") && exists("values")) {
 } else if (exists(ip_inputs) && exists(ip_values)) {
   values <- do.call(reactiveValues, get(ip_values))
   state_list <- get(ip_inputs)
-  rm(list = c(ip_inputs, ip_values), envir = .GlobalEnv)
-  rm(list = ls(pattern = "^RadiantInputs"), envir = .GlobalEnv)
-  rm(list = ls(pattern = "^RadiantValues"), envir = .GlobalEnv)
-  # rm(list = ls(pattern = "^RadiantInputs", envir = .GlobalEnv), envir = .GlobalEnv)
-  # rm(list = ls(pattern = "^RadiantValues", envir = .GlobalEnv), envir = .GlobalEnv)
-  # rm(list = ls(pattern = "^RadiantInputs", envir = .GlobalEnv), inherits = TRUE)
-  # rm(list = ls(pattern = "^RadiantValues", envir = .GlobalEnv), inherits = TRUE)
+  rm(list = c(ip_inputs, ip_values, ip_dump), envir = .GlobalEnv)
 } else {
   state_list <- list()
-  # values <- reactiveValues()
   values <- init_state(reactiveValues())
 }
 
