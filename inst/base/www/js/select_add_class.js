efccba17fe3b2cb1c09d3b0a94ff4ca800e24fd0
mostly-harmless <vnijs@ucsd.edu>
2015-02-06 17:21:13 -0800

    Cleanup

--- inst/base/www/js/select_add_class.js
+++
@@ -1,9 +0,0 @@
-// document.getElementsByTagName("select[multiple]").className += " form-control";
-
-// document.getElementById("vizvars1").className += " form-control";
-// document.getElementById("reg_var2").className += " form-control";
-// document.getElementById("reg_intsel").className += " form-control";
-
-// $('#vizvars1').addClass('form-control');
-
-// $('#data2').addClass('form-control');
