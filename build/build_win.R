fe24d2f58df1d1148922976840096b68b6c39361
Vincent Nijs <vnijs@ucsd.edu>
2015-02-21 23:16:07 -0800

    build github dependencies

--- build/build_win.R
+++ build/build_win.R
@@ -1,11 +1,17 @@
 # run this separately on mac and windows
 # install.packages('devtools')
+
+# next line not working - run from git terminal
+#system("sh C:/Users/vnijs/Desktop/GitHub/radiant_dev/build/git_win.sh")
+
 library(devtools)
 document(roclets=c('rd', 'collate', 'namespace'))
 build('../radiant_dev', binary = TRUE)
+build('../shinyAce', binary = TRUE)
+build('../rpivotTable', binary = TRUE)
 
 setwd('../')
-rfile <- Sys.glob("radiant*zip")
+rfile <- Sys.glob("*zip")
 setwd('radiant_dev')
 
 file.copy(paste0("../",rfile),"Z:/Desktop/GitHub")
