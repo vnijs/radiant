6ea25a9c817e0f9e8d84ab10ed1824ff5aeb4f35
Vincent Nijs <vnijs@ucsd.edu>
2015-02-21 23:16:07 -0800

    build github dependencies

---
+++ build/git_win.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd ~/Desktop/GitHub
+#git clone git@github.com:smartinsightsfromdata/rpivotTable.git
+#git clone git@github.com:trestletech/shinyAce.git
+cd radiant_dev
+git pull
+cd ../shinyAce
+git pull
+cd ../rpivotTable
+git pull
