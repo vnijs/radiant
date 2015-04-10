8415f9c0c20debc6f051725c52a038082a9aec7b
mostly-harmless <vnijs@ucsd.edu>
2015-04-09 18:08:32 -0700

    Trying out DT for the Data > View tab

--- inst/base/tools/data/view.R
+++ inst/base/tools/data/view.R
@@ -1,7 +1,7 @@
 #############################################
 # View table output of the selected dataset
 #############################################
-output$uiView_vars <- renderUI({
+output$ui_view_vars <- renderUI({
   vars <- varnames()
   selectInput("view_vars", "Select variables to show:", choices  = vars,
     selected = state_multiple("view_vars",vars, vars), multiple = TRUE,
@@ -11,30 +11,33 @@ output$uiView_vars <- renderUI({
 output$ui_View <- renderUI({
   list(
     wellPanel(
-      uiOutput("uiView_vars")
+      uiOutput("ui_view_vars")
     ),
     help_modal('View','viewHelp',inclMD(paste0(path,"/base/tools/help/view.md")))
   )
 })
 
-output$dataviewer <- renderDataTable({
+# output$dataviewer <- renderDataTable({
 
-  if(not_available(input$view_vars)) return()
-  select_(.getdata(), .dots = input$view_vars)
+#   if(not_available(input$view_vars)) return()
+#   select_(.getdata(), .dots = input$view_vars)
 
-}, options = list(orderClasses = TRUE, caseInsensitive = TRUE,
-  lengthMenu = list(c(10, 25, 50, -1),c('10','25','50','All')),
-  pageLength = 10, search = list(regex = TRUE)))
+# }, options = list(orderClasses = TRUE, caseInsensitive = TRUE,
+#   lengthMenu = list(c(10, 25, 50, -1),c('10','25','50','All')),
+#   pageLength = 10, search = list(regex = TRUE)))
 
 
 # use DT to add dplyr - server side code
-# output$dataviewer <- DT::renderDataTable({
+output$dataviewer <- DT::renderDataTable({
 
-#   if(not_available(input$view_vars)) return()
-#   select_(.getdata(), .dots = input$view_vars) %>%
-#   DT::datatable(., rownames = FALSE, filter = 'bottom',
-#         options = list(pageLength = 10, autoWidth = TRUE, server = TRUE,
-#                        class = 'cell-border stripe',
-#                        lengthMenu = list(c(10, 25, 50, -1),c('10','25','50','All')))
-#   )
-# })
+  if(not_available(input$view_vars)) return()
+  select_(.getdata(), .dots = input$view_vars) -> dat
+  DT::datatable(dat, rownames = FALSE, filter = "bottom",
+    # server = TRUE,
+    options = list(
+      columnDefs = list(list(className = 'dt-center', targets = "_all")),
+      pageLength = 10,
+      lengthMenu = list(c(10, 25, 50, -1),c('10','25','50','All'))
+    )
+  )
+})
