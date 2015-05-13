a9c62cd8c8a9dd197b1b514d1463a7176a3db142
mostly-harmless <vnijs@ucsd.edu>
2015-05-13 13:20:47 -0700

    Update css for shinyapps.io: `date +"%m-%d-%Y"` [ci skip]

--- build/deploy_shinyapps.R
+++ build/deploy_shinyapps.R
@@ -6,6 +6,8 @@ readLines(fn) %>%
   gsub("top: 95px;", "top: 145px;", .) %>%
   cat(file = fn, sep = "\n")
 
+Sys.sleep(2)
+
 system("git add --all .")
 system("git commit -m 'Update css for shinyapps.io: `date +\"%m-%d-%Y\"` [ci skip]'")
 system("git push")
@@ -18,15 +20,14 @@ system("git push")
 # devtools::install_github(c("rstudio/shinyapps", "themel/sendmailR", "jimhester/covr"))
 devtools::install_github(c("vnijs/radiant","vnijs/DT"))
 
-
 library(shinyapps)
 fpath <- "~/gh/radiant_dev/inst/base"
 setwd(fpath)
 
-for (file in list.files("../../../shinyapps/R", pattern="\\.(r|R)$", full.names = TRUE))
+for (file in list.files("../../../shinyapps/R", pattern = "\\.(r|R)$", full.names = TRUE))
   source(file, local = TRUE)
-source("../../build/deployapp.R", local = TRUE)
 
+source("../../build/deployapp.R", local = TRUE)
 deployApp(account = "vnijs", launch.browser = FALSE)
 
 setwd(file.path(fpath,"../quant"))
@@ -40,18 +41,25 @@ readLines(fn) %>%
   gsub("top: 145px;", "top: 95px;", .) %>%
   cat(file = fn, sep = "\n")
 
+Sys.sleep(2)
+
 system("git add --all .")
 system("git commit -m 'Undo css update for shinyapps.io: `date +\"%m-%d-%Y\"` [ci skip]'")
 system("git push")
 
-
-answ <- readline("Did you create binary package for Windows? (y/n) ")
+answ <- readline("Did you create binary packages for Windows? (y/n) ")
 if (substr(answ, 1, 1) %in% c("y","Y")) {
   setwd("~/gh/radiant_dev/")
   system("sh build/build_mac_win.sh")
 }
 
-rm(list = ls())
-
 # in case of problems
 # shinyapps::showLogs(entries=1000)
+
+system("osascript ~/Desktop/update.scpt")
+
+# for major pull problems
+# git fetch --all
+# git reset --hard origin/master
+
+rm(list = ls())
