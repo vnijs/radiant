ab59b1e182f3b8e30b5d6c5df2c8ebca15f29199
mostly-harmless <vnijs@ucsd.edu>
2015-07-13 02:56:50 -0700

    forcing unlink of state file

--- inst/base/radiant.R
+++ inst/base/radiant.R
@@ -31,7 +31,8 @@ saveStateOnRefresh <- function(session = session) {
       } else {
         if (is.null(input$uploadState)) {
           if (exists("r_sessions")) try(r_sessions[[r_ssuid]] <- NULL, silent = TRUE)
-          unlink("~/r_sessions/r_", r_ssuid, ".rds")
+          # saveRDS(r_sessions[[r_ssuid]], file = paste0("~/r_sessions/r_", r_ssuid, ".rds"))
+          if (!r_local) unlink("~/r_sessions/r_", r_ssuid, ".rds", force = TRUE)
         }
       }
     })
