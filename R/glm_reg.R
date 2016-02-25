90aed1aa81ccd11663b4deeee08b37167ffc24a1
mostly-harmless <vnijs@ucsd.edu>
2016-02-24 21:47:49 -0800

    Update [ci skip]

--- R/glm_reg.R
+++ R/glm_reg.R
@@ -783,10 +783,10 @@ confint_robust <- function (object, parm, level = 0.95, ...) {
         parm <- pnames[parm]
     a <- (1 - level)/2
     a <- c(a, 1 - a)
-    pct <- stats:::format.perc(a, 3)
+    # pct <- format.perc(a, 3)
     fac <- qnorm(a)
-    ci <- array(NA, dim = c(length(parm), 2L), dimnames = list(parm,
-        pct))
+    # ci <- array(NA, dim = c(length(parm), 2L), dimnames = list(parm, pct))
+    ci <- array(NA, dim = c(length(parm), 2L), dimnames = list(parm, c("Low","High")))
     ses <- sqrt(diag(sandwich::vcovHC(object)))[parm]
     ci[] <- cf[parm] + ses %o% fac
     ci
