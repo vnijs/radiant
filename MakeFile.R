f65cdc20e01690cdab427328000b264d743fe65f
vnijs <vnijs@ucsd.edu>
2016-03-24 20:41:31 -0700

    Update menu structure

--- MakeFile.R
+++ MakeFile.R
@@ -1,9 +1,8 @@
 library(knitr)
 library(dplyr)
 setwd("~/gh/radiant_gh-pages/")
-# file.copy("../radiant/inst/marketing/data/shopping.rda",
-#           "~/Desktop/shopping.rda",overwrite = TRUE)
 
+## generate (updated) html and md files
 system('make')
 setwd('base/app')
 list.files(pattern = "*.Rmd") %>% sapply(knit)
@@ -12,12 +11,10 @@ setwd('../marketing'); system('make')
 setwd('../analytics'); system('make')
 setwd('../quant/app');
 list.files(pattern = "*.Rmd") %>% sapply(knit)
-# knitr::knit('dtree.Rmd')
 setwd('../../quant'); system('make')
 setwd('../')
 
 ## making the README.md file after clean-up
-
 knit("README.Rmd")
 setwd("sub")
 knit("README_dev.Rmd")
@@ -27,7 +24,7 @@ knit("tutorials_dev.Rmd")
 file.copy("tutorials_dev.md","../../radiant/inst/base/tools/app/tutorials.md",overwrite = TRUE)
 setwd('../')
 
-# unlink("~/Desktop/shopping.rda")
+## sync (R)md files to gh/radiant
 system('sh rsync_base2app.sh')
 
 ## create documentation pdf
---
+++ quant/app/sample_size_comp.md
@@ -0,0 +1,2 @@
+> Determine the required sample size for comparisons of means or proportions
+
